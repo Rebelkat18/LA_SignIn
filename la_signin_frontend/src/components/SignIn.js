b8c3d931e240cd07b78d433fe687c31c67ef4a28
Kat <91990952+Rebelkat18@users.noreply.github.com>
2024-04-15 22:48:36 -0400

    trying stuff

--- la_signin_frontend/src/components/SignIn.js
+++ la_signin_frontend/src/components/SignIn.js
@@ -2,8 +2,37 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import { useState } from 'react';
+import axios from 'axios';
 
 function SignIn() {
+    //Form States
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [studentID, setStudentID] = useState('');
+    const [classID, setClassID] = useState('');
+    const [timeIn, setTimeIn] = useState('');
+    const [athlete, setAthlete] = useState(false);
+
+    //Form Submit Handler
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Form Submitted');
+        console.log(firstName, lastName, studentID, classID, timeIn, athlete);
+        // axios.post('http://localhost:5000/api/signin', {
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     studentID: studentID,
+        //     classID: classID,
+        //     timeIn: timeIn,
+        //     athlete: athlete
+        // }).then((response) => {
+        //     console.log(response);
+        // }).catch((error) => {
+        //     console.log(error);
+        // });
+    }
+
     return (
         <div className='container'>
             <div className="d-flex justify-content-center">
@@ -11,22 +40,22 @@ function SignIn() {
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridFirstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="First Name" />
+                            <Form.Control type="text" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridLastName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" placeholder="Last Name" />
+                            <Form.Control type="text" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} value={lastName} />
                         </Form.Group>
                     </Row>
 
                     <Form.Group className="mb-3" controlId="formGridStudentID">
                         <Form.Label>Student ID</Form.Label>
-                        <Form.Control placeholder="0000000" />
+                        <Form.Control placeholder="0000000" onChange={(e) => setStudentID(e.target.value)} value={studentID} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" id="formGridClass">
-                        <Form.Select aria-label="Default select example">
+                        <Form.Select aria-label="Default select example" onChange={(e) => setClassID(e.target.value)} value={classID}>
                             <option>Select your class from the dropdown</option>
                             <option value="1">MATH 119</option>
                             <option value="2">MATH 119A</option>
@@ -36,19 +65,21 @@ function SignIn() {
 
                     <Form.Group className='mb-3' controlId="formGridTimeIn">
                         <Form.Label>Time In</Form.Label>
-                        <Form.Control type="time" />
+                        <Form.Control type="time" onChange={(e) => setTimeIn(e.target.value)} value={timeIn} />
                     </Form.Group>
 
-                    <div className="d-flex justify-content-center">
-                        <Form.Group className='mb-3' controlId="formGridAthelete">
-                            <Form.Label>Athelete?</Form.Label>
-                            <Form.Check type="radio" label="Yes" />
-                            <Form.Check type="radio" label="No" checked />
-                        </Form.Group>
-                    </div>
+
+                    <Form.Group className='mb-3' controlId="formGridAthelete">
+                        <Form.Select aria-label="Default select example" onChange={(e) => setAthlete(e.target.value)} value={athlete}>
+                            <option>Are you an athelete?</option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
+                        </Form.Select>
+                    </Form.Group>
+
 
                     <div className="d-flex justify-content-center">
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>
                             Submit
                         </Button>
                     </div>
